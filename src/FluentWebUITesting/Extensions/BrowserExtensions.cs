d8ca30d17c6e02db0e65fbb45900e5e53106eaf1
Shashank Shetty <sshetty@mvbalaw.com>
2010-06-08 21:46:59 -0500

    Button Visible text now scans value as well as text of a button, added dialoghandler

--- src/FluentWebUITesting/Extensions/BrowserExtensions.cs
+++ src/FluentWebUITesting/Extensions/BrowserExtensions.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Linq;
 using System.Threading;
-
 using FluentWebUITesting.Accessors;
 using FluentWebUITesting.Controls;
-
 using JetBrains.Annotations;
-
 using WatiN.Core;
 
 namespace FluentWebUITesting.Extensions
@@ -16,51 +13,57 @@ namespace FluentWebUITesting.Extensions
 		public static ButtonWrapper ButtonWithVisibleText(this Browser browser, [NotNull] string text)
 		{
 			const string buttonWithLabel = "button with visible text '{0}'";
-			return new ButtonWrapper(browser.Button(Find.ByValue(text)), String.Format(buttonWithLabel, text));
+			return new ButtonWrapper(browser.Button(Find.ByValue(text).Or(Find.ByText(text))), String.Format(buttonWithLabel, text));
 		}
 
 		public static CheckBoxWrapper CheckBoxWithId(this Browser browser, [NotNull] string id)
 		{
 			const string checkBoxWithId = "checkbox with id '{0}'";
-			var checkBox = browser.CheckBoxes.FirstOrDefault(x=>x.Id == id)??browser.CheckBox(Find.ById(id));
+			var checkBox = browser.CheckBoxes.FirstOrDefault(x => x.Id == id) ?? browser.CheckBox(Find.ById(id));
 			return new CheckBoxWrapper(checkBox, String.Format(checkBoxWithId, id));
 		}
 
 		public static CheckBoxWrapper CheckBoxWithIdAndValue(this Browser browser, [NotNull] string id, [NotNull] string value)
 		{
 			const string message = "checkbox with id '{0}' and value '{1}'";
-			var checkBox = browser.CheckBoxes.FirstOrDefault(x=>x.Id == id && x.GetAttributeValue("value") == value)??browser.CheckBox(Find.ById(id));
+			var checkBox = browser.CheckBoxes.FirstOrDefault(x => x.Id == id && x.GetAttributeValue("value") == value) ??
+			               browser.CheckBox(Find.ById(id));
 			return new CheckBoxWrapper(checkBox, String.Format(message, id, value));
 		}
 
 		public static DivWrapper DivWithId(this Browser browser, [NotNull] string id)
 		{
-			const string divWithID = "div with id '{0}'";
-			return new DivWrapper(browser.Div(Find.ById(id)), String.Format(divWithID, id));
+			const string divWithId = "div with id '{0}'";
+			return new DivWrapper(browser.Div(Find.ById(id)), String.Format(divWithId, id));
 		}
 
 		public static DropDownListWrapper DropDownListWithId(this Browser browser, [NotNull] string idOfList)
 		{
-			const string dropDownListWithID = "drop down list with id '{0}'";
+			const string dropDownListWithId = "drop down list with id '{0}'";
 			return new DropDownListWrapper(browser.SelectList(Find.ById(idOfList)),
-			                               String.Format(dropDownListWithID, idOfList));
+			                               String.Format(dropDownListWithId, idOfList));
 		}
 
 		public static FileDownloadHandlerWrapper FileDownload(this Browser browser, Action action)
 		{
 			return new FileDownloadHandlerWrapper(browser, action);
 		}
+		
+		public static DialogHandlerWrapper Dialog(this Browser browser, Action action)
+		{
+			return new DialogHandlerWrapper(browser, action);
+		}
 
 		public static LabelWrapper LabelWithId(this Browser browser, [NotNull] string id)
 		{
-			const string labelWithID = "label with id '{0}'";
-			return new LabelWrapper(browser.Label(Find.ById(id)), String.Format(labelWithID, id));
+			const string labelWithId = "label with id '{0}'";
+			return new LabelWrapper(browser.Label(Find.ById(id)), String.Format(labelWithId, id));
 		}
 
 		public static LinkWrapper LinkWithId(this Browser browser, [NotNull] string id)
 		{
-			const string linkWithID = "link with id '{0}'";
-			return new LinkWrapper(browser.Link(Find.ById(id)), String.Format(linkWithID, id));
+			const string linkWithId = "link with id '{0}'";
+			return new LinkWrapper(browser.Link(Find.ById(id)), String.Format(linkWithId, id));
 		}
 
 		public static LinkWrapper LinkWithVisibleText(this Browser browser, [NotNull] string text)
@@ -76,20 +79,21 @@ namespace FluentWebUITesting.Extensions
 
 		public static RadioButtonOptionWrapper RadioButtonOptionWithId(this Browser browser, [NotNull] string idOfOption)
 		{
-			const string radioButtonOptionWithID = "radio button option with id '{0}'";
-			return new RadioButtonOptionWrapper(browser.RadioButton(Find.ById(idOfOption)), String.Format(radioButtonOptionWithID, idOfOption));
+			const string radioButtonOptionWithId = "radio button option with id '{0}'";
+			return new RadioButtonOptionWrapper(browser.RadioButton(Find.ById(idOfOption)),
+			                                    String.Format(radioButtonOptionWithId, idOfOption));
 		}
 
 		public static SpanWrapper SpanWithId(this Browser browser, [NotNull] string id)
 		{
-			const string spanWithID = "span with id '{0}'";
-			return new SpanWrapper(browser.Span(Find.ById(id)), String.Format(spanWithID, id));
+			const string spanWithId = "span with id '{0}'";
+			return new SpanWrapper(browser.Span(Find.ById(id)), String.Format(spanWithId, id));
 		}
 
 		public static TableWrapper TableWithId(this Browser browser, [NotNull] string id)
 		{
-			const string tableWithID = "table with id '{0}'";
-			return new TableWrapper(browser.Table(Find.ById(id)), String.Format(tableWithID, id));
+			const string tableWithId = "table with id '{0}'";
+			return new TableWrapper(browser.Table(Find.ById(id)), String.Format(tableWithId, id));
 		}
 
 		public static ReadOnlyText Text(this Browser browser)
@@ -99,9 +103,9 @@ namespace FluentWebUITesting.Extensions
 
 		public static TextBoxWrapper TextBoxWithId(this Browser browser, [NotNull] string id)
 		{
-			const string textBoxWithID = "text box with id '{0}'";
+			const string textBoxWithId = "text box with id '{0}'";
 			return new TextBoxWrapper(browser
-			                          	.TextField(Find.ById(id)), String.Format(textBoxWithID, id));
+			                          	.TextField(Find.ById(id)), String.Format(textBoxWithId, id));
 		}
 	}
 }
