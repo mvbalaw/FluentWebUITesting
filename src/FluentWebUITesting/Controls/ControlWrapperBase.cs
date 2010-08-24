ed35e34b78ea1952d34bdcc690e0ac0e213567af
Kathy Coats <kcoats@mvbalaw.com>
2010-08-24 11:20:13 -0500

    Add support for .visible on wrapped controls

--- src/FluentWebUITesting/Controls/ControlWrapperBase.cs
+++ src/FluentWebUITesting/Controls/ControlWrapperBase.cs
@@ -1,43 +1,67 @@
 using System;
-
 using FluentWebUITesting.Accessors;
-
 using WatiN.Core;
 
 namespace FluentWebUITesting.Controls
 {
-	public abstract class ControlWrapperBase
-	{
-		protected ControlWrapperBase(string howFound)
-		{
-			HowFound = howFound;
-		}
+    public abstract class ControlWrapperBase
+    {
+        protected ControlWrapperBase(string howFound)
+        {
+            HowFound = howFound;
+        }
 
-		protected abstract Element Element { get; }
-		protected string HowFound { get; private set; }
+        protected abstract Element Element { get; }
+        protected string HowFound { get; private set; }
 
-		public IReadOnlyBooleanState Enabled()
-		{
-			const string unexpectedlyFalse = "{0} is not enabled but should be.";
-			const string unexpectedlyTrue = "{0} is enabled but should not be.";
-			string unexpectedlyTrueMessage = String.Format(unexpectedlyTrue, HowFound);
-			string unexpectedlyFalseMessage = String.Format(unexpectedlyFalse, HowFound);
-			var result = new BooleanState(unexpectedlyFalseMessage,
-			                              unexpectedlyTrueMessage, 
-										  () => Element.Enabled);
-			return result;
-		}
+        public IReadOnlyBooleanState Enabled()
+        {
+            const string unexpectedlyFalse = "{0} is not enabled but should be.";
+            const string unexpectedlyTrue = "{0} is enabled but should not be.";
+            string unexpectedlyTrueMessage = String.Format(unexpectedlyTrue, HowFound);
+            string unexpectedlyFalseMessage = String.Format(unexpectedlyFalse, HowFound);
+            var result = new BooleanState(unexpectedlyFalseMessage,
+                                          unexpectedlyTrueMessage,
+                                          () => Element.Enabled);
+            return result;
+        }
 
-		public IReadOnlyBooleanState Exists()
-		{
-			const string unexpectedlyFalse = "{0} does not exist but should.";
-			const string unexpectedlyTrue = "{0} exists but should not.";
-			string unexpectedlyTrueMessage = String.Format(unexpectedlyTrue, HowFound);
-			string unexpectedlyFalseMessage = String.Format(unexpectedlyFalse, HowFound);
-			var result = new BooleanState(unexpectedlyFalseMessage,
-			                              unexpectedlyTrueMessage,
-										  () => Element.Exists);
-			return result;
-		}
-	}
+        public IReadOnlyBooleanState Exists()
+        {
+            const string unexpectedlyFalse = "{0} does not exist but should.";
+            const string unexpectedlyTrue = "{0} exists but should not.";
+            string unexpectedlyTrueMessage = String.Format(unexpectedlyTrue, HowFound);
+            string unexpectedlyFalseMessage = String.Format(unexpectedlyFalse, HowFound);
+            var result = new BooleanState(unexpectedlyFalseMessage,
+                                          unexpectedlyTrueMessage,
+                                          () => Element.Exists);
+            return result;
+        }
+
+        public IReadOnlyBooleanState Visible()
+        {
+            const string unexpectedlyFalse = "{0} is not visible but should be.";
+            const string unexpectedlyTrue = "{0} is visible but should not be.";
+            string unexpectedlyTrueMessage = String.Format(unexpectedlyTrue, HowFound);
+            string unexpectedlyFalseMessage = String.Format(unexpectedlyFalse, HowFound);
+            var result = new BooleanState(unexpectedlyFalseMessage,
+                                          unexpectedlyTrueMessage,
+                                          () => IsDisplayed(Element));
+            return result;
+        }
+
+        // source http://blog.coditate.com/2009/07/determining-html-element-visibility.html
+        private static bool IsDisplayed(Element element)
+        {
+            if (string.Equals(element.Style.Display, "none"))
+            {
+                return false;
+            }
+            if (element.Parent != null)
+            {
+                return IsDisplayed(element.Parent);
+            }
+            return true;
+        }
+    }
 }
