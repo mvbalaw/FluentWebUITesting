9249186f951102632ee103867cacc89422ce6b32
Clinton Sheppard <gar3ts@gmail.com>
2011-10-03 15:56:03 -0500

    - handle null source in Text.StartsWith and Text.Contains - correct StartsWith and Contains error message text (were swapped)

--- src/FluentWebUITesting/Accessors/TextBase.cs
+++ src/FluentWebUITesting/Accessors/TextBase.cs
@@ -18,13 +18,13 @@ namespace FluentWebUITesting.Accessors
 
         public IReadOnlyBooleanState Contains([NotNull] string containedText)
         {
-            const string unexpectedlyTrue = "The text '{0}' in {1} should not start with '{2}'";
-            const string unexpectedlyFalse = "The text '{0}' in {1} should start with '{2}'";
+            const string unexpectedlyTrue = "The text '{0}' in {1} should not contain '{2}'";
+            const string unexpectedlyFalse = "The text '{0}' in {1} should contain '{2}'";
             string unexpectedlyFalseMessage = String.Format(unexpectedlyFalse, Text, _howFound, containedText);
             string unexpectedlyTrueMessage = String.Format(unexpectedlyTrue, Text, _howFound, containedText);
             var result = new BooleanState(unexpectedlyFalseMessage,
                                           unexpectedlyTrueMessage,
-                                          () => Text.Contains(containedText));
+                                          () => Text != null && Text.Contains(containedText));
             return result;
         }
 
@@ -61,13 +61,13 @@ namespace FluentWebUITesting.Accessors
 
         public IReadOnlyBooleanState StartsWith([NotNull] string startingText)
         {
-            const string unexpectedlyTrue = "The text '{0}' in {1} should not contain '{2}'";
-            const string unexpectedlyFalse = "The text '{0}' in {1} should contain '{2}'";
+            const string unexpectedlyTrue = "The text '{0}' in {1} should not start with '{2}'";
+            const string unexpectedlyFalse = "The text '{0}' in {1} should start with '{2}'";
             var unexpectedlyTrueMessage = String.Format(unexpectedlyTrue, Text, _howFound, startingText);
             var unexpectedlyFalseMessage = String.Format(unexpectedlyFalse, Text, _howFound, startingText);
             var result = new BooleanState(unexpectedlyFalseMessage,
                                           unexpectedlyTrueMessage,
-                                          () => Text.StartsWith(startingText));
+										  () => Text != null && Text.StartsWith(startingText));
             return result;
         }
 
